6920dae32b1ec37f929ef0c143e906d05d6ab09d
Andrey <andreylyapinpolotsk@gmail.com>
2022-04-14 16:01:47 +0300

    Создание класса

---
+++ ПРАКТИКА_1/КЛАСС.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛАБА20._2
+{
+    class Файл
+    {
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public override string ToString()
+        {
+            return Name + Size + Date + Time;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Файл objAsФайл = obj as Файл;
+            if (objAsФайл == null) return false;
+            else return Equals(objAsФайл);
+        }
+        public override int GetHashCode()
+        {
+            return Size;
+        }
+
+        public bool Equals(Файл other)
+        {
+            if (other == null) return false;
+            return (this.Name.Equals(other.Name));
+        }
+    }
+}
